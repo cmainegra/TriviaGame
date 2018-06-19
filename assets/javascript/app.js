db60aa99dd5263a3ccff1a082a77b08fab20dfe4
Charlie <cmainegra@protonmail.com>
2018-06-19 00:09:31 -0400

    htmlandreset

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//timer 90 seconds counting down.
+//display timer on pageload
+    //front and center
+//display questions in the DOM
+    // array of questions
+    // four choices per question
+    // capture and save user response
+    // when user hits done, display to the dom the right and wrong questions
+//
+//
+//
+//
+//
